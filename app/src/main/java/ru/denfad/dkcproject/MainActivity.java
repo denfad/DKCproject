5f1580a30bde96873b2e9d89f90f5935ea1d5fd6
denfad <denfad2003@mail.ru>
2020-10-17 14:47:09 +0300

    add biblioteca

--- app/src/main/java/ru/denfad/dkcproject/MainActivity.java
+++ app/src/main/java/ru/denfad/dkcproject/MainActivity.java
@@ -2,8 +2,17 @@ package ru.denfad.dkcproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CalendarView;
+import android.widget.Toast;
+
+import sun.bob.mcalendarview.MCalendarView;
+import sun.bob.mcalendarview.MarkStyle;
+import sun.bob.mcalendarview.listeners.OnDateClickListener;
+import sun.bob.mcalendarview.listeners.OnMonthChangeListener;
+import sun.bob.mcalendarview.vo.DateData;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +21,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CalendarView calendar = findViewById(R.id.calendar);
+
+        MCalendarView calendarView = findViewById(R.id.calendar);
+
+        calendarView.setMarkedStyle(MarkStyle.BACKGROUND, Color.RED);
+
+        calendarView.markDate(2020, 10, 7);
+        calendarView.markDate(2020, 10, 8);
+
+        calendarView.setOnMonthChangeListener(new OnMonthChangeListener() {
+            @Override
+            public void onMonthChange(int year, int month) {
+                Toast.makeText(MainActivity.this, String.format("%d-%d", year, month), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        calendarView.setOnDateClickListener(new OnDateClickListener() {
+            @Override
+            public void onDateClick(View view, DateData date) {
+                Toast.makeText(MainActivity.this, String.format("%d-%d", date.getMonth(), date.getDay()), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        calendarView.hasTitle(false);
+
     }
 }
